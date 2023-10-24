0bcd1e9431011a180602419e800e57bf6b2e76a6
Zhichen Gu <46347956+ZhichenGu@users.noreply.github.com>
2023-10-24 18:13:00 +0100

    Update ofApp.h

--- videoPlayer/src/ofApp.h
+++ videoPlayer/src/ofApp.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxOsc.h"
+
+#define PORT 9999
 
 class ofApp : public ofBaseApp{
 
@@ -10,9 +13,15 @@ class ofApp : public ofBaseApp{
 		void draw();
 
 		void keyPressed(int key);
-    void videoPlaying(ofVideoPlayer videoPlayer);
+        void videoPlaying(ofVideoPlayer videoPlayer);
+        void videoStop(ofVideoPlayer videoPlayer1, ofVideoPlayer videoPlayer2, ofVideoPlayer videoPlayer3,
+                       ofVideoPlayer videoPlayer4, ofVideoPlayer videoPlayer5, ofVideoPlayer videoPlayer6,
+                       ofVideoPlayer videoPlayer7, ofVideoPlayer videoPlayer8, ofVideoPlayer videoPlayer9);
     
     ofVideoPlayer videoPlayer0, videoPlayer1, videoPlayer2, videoPlayer3, videoPlayer4,
                   videoPlayer5, videoPlayer6, videoPlayer7, videoPlayer8, videoPlayer9;
+    
+    ofxOscReceiver receiver;
+    int number;
 		
 };
